eb52374af201ecad0be50cd3e908f446ce4ee514
Methamorphe <methamorphe@gmail.com>
2019-05-04 23:21:24 +0200

    Modif titre, affichage centré en haut

--- app/views/header.tpl.php
+++ app/views/header.tpl.php
@@ -10,8 +10,10 @@
     </head>
 <body>
     <header>
-    <nav class="navbar navbar-light sticky-top">
-    <div id="volet">
+        <div class="d-flex justify-content-center bd-highlight">
+            <h1 class="text-center mr-4 font-weight-bold">Snake By Rocket © </h1>
+        </div>
+        <div id="volet">
             <p>Les règles sont simples : Vous avez <strong>3 vies</strong> seulement.</p>
             <p>A la fin de vos 3 essais, le jeu s'arrête et vous pourrez voir vos résultats et ceux des collègues de la promo.</p>
             <i class="fas fa-user-graduate fa-2x font-weight-bold text-white mb-3"></i>
@@ -19,10 +21,7 @@
             <a href="#volet" class="ouvrir">RULES !</a>
             <a href="#volet_clos" class="fermer text-light bg-dark font-weight-bold">Close Here</a>
         </div>
-        <div class="d-flex flex-row-reverse bd-highlight">
-            <h1 class="text-center-right mr-4 font-weight-bold">Snake By Rocket © </h1>
-        </div>
-    </nav>
+
 </header>
 
 
